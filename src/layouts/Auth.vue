cff951bd9a5ea05263dd01c73c59255b90bfd9c4
ScriptMint <hello@scriptmintsolution.com>
2022-02-05 22:22:17 +0530

    Update Auth.vue

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -6,6 +6,13 @@
             <AppHeader></AppHeader>
 
             <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+                <div class="pt-4 px-4 bg-black">
+                    <p class="px-10 py-2 rounded-full bg-info text-info-label">
+                        <a href="https://scriptmint.com/vana-admin" target="_blank">
+                            Vumin is FREE Admin Template built with Vue.js & Tailwind CSS. Checkout our Advanced Version Vana-Admin built with Laravel, Vue.js & Tailwind CSS at https://scriptmint.com/vana-admin
+                        </a>
+                    </p>
+                </div>
                 <div class="space-y-4">
                     <router-view />
                 </div>
